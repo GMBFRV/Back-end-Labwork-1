81ff05996f34515b86d6b84769224461a3a7080b
Gadiev Renat <gadievforever@gmail.com>
2023-10-20 16:54:13 +0300

    [Модифіковано файл views.py]

--- project/views.py
+++ project/views.py
@@ -1,1 +1,23 @@
+from flask import Flask, jsonify, redirect, url_for
+from datetime import datetime
 
+app = Flask(__name__)
+
+@app.route('/healthcheck', methods=['GET'])
+def healthcheck():
+    current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    status = 'Service is up and running'
+    greeting = 'Hello, user!'
+    response_data = {
+        'timestamp': current_time,
+        'status': status,
+        'greeting': greeting
+    }
+    return jsonify(response_data), 200
+
+@app.route('/', methods=['GET'])
+def welcome():
+    return 'Welcome, user! <a href="/healthcheck">Go to Healthcheck</a>'
+
+if __name__ == '__main__':
+    app.run()
